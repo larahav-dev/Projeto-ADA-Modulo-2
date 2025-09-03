9df8dcae0909f332b634f95b438846ab3fc6bf14
alvarofl <alvarolima837@gmail.com>
2025-09-02 21:34:24 -0300

    cria a classe cliente model, e cria a classe map clienteRepository vai ser armazenada o map

--- Projeto/src/model/Cliente.java
+++ Projeto/src/model/Cliente.java
@@ -1,4 +1,35 @@
-package model;
+        package model;
+        
+
+                
+
+import java.util.UUID;
 
 public class Cliente {
+                        private  UUID id;
+        private String nome;
+        private String email;
+                private String documento;
+                                
+                public Cliente(String nome, String email, String documento) {
+
+
+                        this.id = UUID.randomUUID();
+                                this.nome = nome;
+                                        this.email = email;
+                                                this.documento = documento;
+                        }
+    public void atualizarDados(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }   
+
+    
+    
+    public UUID getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getDocumento() { return documento; }
+
+    
 }
