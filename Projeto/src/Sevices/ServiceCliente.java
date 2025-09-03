e552a87c7983fdc6370fe36823ab7976527354be
alvarofl <alvarolima837@gmail.com>
2025-09-03 14:28:56 -0300

    construindo  o método servicesCliente

--- Projeto/src/Sevices/ServiceCliente.java
+++ Projeto/src/Sevices/ServiceCliente.java
@@ -1,4 +1,32 @@
-package Sevices;
-
+        package Sevices;
+import model.Cliente;
+import repository.ClienteRepository;
+import java.util.UUID;
+import java.util.List;
 public class ServiceCliente {
+private final ClienteRepository clienteRepository;
+    public ServiceCliente (ClienteRepository clienteRepository) {
+            this.clienteRepository = clienteRepository;
+    }
+public void cadastrarCliente(Cliente cliente) {
+        clienteRepository.salvar(cliente);
 }
+
+                    
+public Cliente buscarCliente(UUID id) {
+    return clienteRepository.buscarPorId(id);
+}
+public void atualisarCliente(UUID id, String novoNome, String novoEmail) {
+            Cliente cliente = buscarCliente(id);
+            //verifica se o cliente existe
+ }
+public List<Cliente> listarClientes(){
+    return clienteRepository.listarTodos();
+}
+
+            
+
+    }
+
+
+
--- Projeto/src/model/Pedido.java
+++ Projeto/src/model/Pedido.java
@@ -1,5 +1,5 @@
 package model;
-
+        
 import java.time.LocalDate;
 
 public class Pedido {
